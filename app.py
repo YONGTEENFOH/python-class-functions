d5d25c49e4bcf33be7130cf3c3d1250fb1c01ce2
YONG TEEN FOH <56257279+YONGTEENFOH@users.noreply.github.com>
2020-08-18 17:18:19 +0800

    Added comments

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ guitar1 = guitar("yamaha", 350, "good")
 guitar2 = guitar("ibanez", 150, "bad")
 guitar3 = guitar("gibson", 200, "good")
 
+#class function can be used by objects to choose the best guitar based on 2 conditions
 print(guitar3.less_than_RM200_and_good())
--- guitar.py
+++ guitar.py
@@ -4,6 +4,7 @@ class guitar:
         self.price = price
         self.quality = quality
 
+#functions created to find the best value guitar
     def less_than_RM200_and_good(self):
         if self.price <= 200 and self.quality == "good":
             return True
